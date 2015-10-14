2f053c726d10a45a4862d1bfae8cc4edfc2ff878
J. Anthony Sterrett, Jr <jasterrett@gmail.com>
2015-10-14 14:25:14 -0400

    add opencv function

--- penrose/main.cpp
+++ penrose/main.cpp
@@ -1,6 +1,11 @@
 #include "penrose.h"
 
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+
 #include <cmath>
+#include <iostream>
 
 auto penrose_sun (
     const point& center,
@@ -32,7 +37,35 @@ std::tuple <
       std::set<half_dart> {});
 }
 
+auto draw (
+    cv::Mat& m, const half_kite& hk, 
+    const cv::Scalar& color = CV_RGB(0, 0, 0)) ->
+cv::Mat&
+{
+  cv::Point a (hk.a.real(), hk.a.imag()),
+            b (hk.b.real(), hk.b.imag()),
+            c (hk.c.real(), hk.c.imag());
+
+  cv::line(m, b, c, color);
+  cv::line(m, c, a, color);
+
+  return m;
+}
+
 int main()
 {
+  std::set<half_kite> half_kites;
+  std::set<half_dart> half_darts;
+
+  std::tie(half_kites, half_darts) = penrose_sun(point(256, 256), 256);
+
+  cv::Mat img = cv::Mat::zeros(512, 512, CV_8UC3);
+
+  for (const auto& hk : half_kites) {
+    draw(img, hk);
+  }
+
+  cv::imwrite("penrose.png", img);
+
   return 0;
 }
